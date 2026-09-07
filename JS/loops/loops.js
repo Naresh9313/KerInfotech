b2d0629246bc3b7493b63fc0f201fdb820329409
Naresh <nareshprajapati9313@gmail.com>
2026-09-07 19:06:13 +0530

    kerinfotech task

---
+++ JS/loops/loops.js
@@ -0,0 +1,37 @@
+
+// //Print numbers from 1–100
+// for (let i = 1; i <= 100; i++) {
+//     console.log(i);
+// }
+
+//Print even numbers
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+// //Print odd numbers
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 !== 0) {
+//         console.log(i);
+//     }
+// }
+
+
+//Calculate the sum of numbers
+// let sum = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//     sum = sum + i;
+// }
+
+// console.log(sum);
+
+
+//Create multiplication tables
+for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${i} x ${j} = ${i * j}`);
+    }
+}

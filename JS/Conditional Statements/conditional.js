8f401c9e732e99926723ea2c47e024a8c9f387fb
Naresh <nareshprajapati9313@gmail.com>
2026-09-08 12:33:33 +0530

    done

--- JS/Conditional Statements/conditional.js
+++ JS/Conditional Statements/conditional.js
@@ -1,22 +1,40 @@
+// //if & else statement
+// let age = 20
 
-//if & else statement
-let age = 20
+// if(age >= 50){
+//     console.log("You are  eligible to vote.");
+// }
+// else{
+//     console.log("You are not eligible to vote.");
+// }
 
-if(age >= 50){
-    console.log("You are  eligible to vote.");
-}
-else{
-    console.log("You are not eligible to vote.");
-}
+// const marks = 75;
 
-const marks = 75;
+// if (marks >= 90) {
+//     console.log("A Grade");
+// } else if (marks >= 74) {
+//     console.log("B Grade");
+// } else if (marks >= 50) {
+//     console.log("C Grade");
+// } else {
+//     console.log("Fail");
+// }
 
-if (marks >= 90) {
+let marks = 90;
+
+switch (true) {
+  case marks >= 94:
     console.log("A Grade");
-} else if (marks >= 74) {
+    break;
+  case marks >= 95:
     console.log("B Grade");
-} else if (marks >= 50) {
+    break;
+  case marks >= 96:
     console.log("C Grade");
-} else {
+    break;
+  case marks >= 80:
+    console.log("D Grade");
+    break;
+  default:
     console.log("Fail");
 }
